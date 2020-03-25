1ee688cf239a5b64efb7064993aae83747c03df8
sabativi <victor.sabatier@gmail.com>
2020-03-25 08:52:04 +0100

    add geolocation

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import circleToPolygon from "circle-to-polygon";
 import ReactMapboxGl, { GeoJSONLayer, Marker } from "react-mapbox-gl";
 import AlgoliaPlaces from "algolia-places-react";
+import useGeolocation from "@rooks/use-geolocation";
 import styled from "styled-components";
-
-import "./App.css";
+import Grid from "styled-components-grid";
 
 const circleLayout = { visibility: "visible" };
 const circlePaint = {
-  "circle-color": "red"
+  "circle-color": "rgb(11, 84, 175)"
 };
 
 const createCircularPolygon = (center, radiusKm) => {
@@ -18,11 +18,24 @@ const createCircularPolygon = (center, radiusKm) => {
 const parisCoordinates = [2.35779, 48.8718];
 
 const Mark = styled.div`
-  background-color: #e74c3c;
+  background-color: rgb(11, 84, 175);
   border-radius: 50%;
   width: 20px;
   height: 20px;
-  border: 4px solid #eaa29b;
+  border: 4px solid #5da0f4;
+`;
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid rgb(11, 84, 175);
+  color: rgb(11, 84, 175);
+  margin: 0.5em 1em;
+  padding: 1em 1em;
+`;
+
+const Container = styled.div`
+  text-align: center;
 `;
 
 function App() {
@@ -31,36 +44,64 @@ function App() {
       "pk.eyJ1Ijoic2FiYXRpdmkiLCJhIjoiY2dLbmxVcyJ9.LJkwUe2kx2yEap2Exx8M9A"
   });
   const [radiusCenter, setRadiusCenter] = React.useState(null);
+  const [when, setWhen] = React.useState(false);
+
+  const geoObj = useGeolocation({ when });
+
+  React.useEffect(() => {
+    if (!!geoObj) {
+      setRadiusCenter(geoObj);
+    }
+  }, [geoObj]);
 
   return (
-    <div className="App">
-      <h3>Pour connaitre jusqu'où vous pouvez aller dans votre quartier</h3>
-      <h6>
-        L'état autorise une sortie jusqu'à 1 km autour de chez vous, Visualiser
-        le sur la carte
-      </h6>
-      <div style={{ width: "80%", margin: "auto", marginBottom: 40 }}>
-        <AlgoliaPlaces
-          placeholder="Rentrer votre adresse"
-          options={{
-            appId: "plIOAJ96YX8Q",
-            apiKey: "a6659aa3595deea11ca0374727609432",
-            language: "fr",
-            countries: ["fr"],
-            type: "address",
-            useDeviceLocation: true
-          }}
-          onClear={() => setRadiusCenter(null)}
-          onChange={({ suggestion }) => {
-            const { latlng } = suggestion;
-            setRadiusCenter(latlng);
-          }}
-          onError={({ message }) => {
-            console.log(message);
-            alert("Erreur");
-          }}
-        />
-      </div>
+    <div>
+      <Container>
+        <h3>Pour connaitre jusqu'où vous pouvez aller dans votre quartier</h3>
+        <h6>
+          L'état autorise une sortie jusqu'à 1 km autour de chez vous,
+          Visualiser le sur la carte
+        </h6>
+      </Container>
+      <Grid horizontalAlign="center" verticalAlign="center">
+        <Grid.Unit
+          size={{ tablet: 1, desktop: 2 / 3 }}
+          style={{ marginLeft: 30, marginRight: 30, margin: "auto" }}
+        >
+          <AlgoliaPlaces
+            placeholder="Rentrer votre adresse"
+            options={{
+              appId: "plIOAJ96YX8Q",
+              apiKey: "a6659aa3595deea11ca0374727609432",
+              language: "fr",
+              countries: ["fr"],
+              type: "address",
+              useDeviceLocation: true
+            }}
+            onClear={() => setRadiusCenter(null)}
+            onChange={({ suggestion }) => {
+              const { latlng } = suggestion;
+              setRadiusCenter(latlng);
+            }}
+            onError={({ message }) => {
+              alert("Erreur");
+            }}
+          />
+        </Grid.Unit>
+        <Grid.Unit size={{ tablet: 1, desktop: 1 / 4 }}>
+          <h6>
+            Ou
+            <Button
+              onClick={() => {
+                setRadiusCenter(null);
+                setWhen(true);
+              }}
+            >
+              Geolocaliser moi
+            </Button>
+          </h6>
+        </Grid.Unit>
+      </Grid>
       <Map
         // eslint-disable-next-line
         style="mapbox://styles/mapbox/streets-v9"
@@ -71,7 +112,7 @@ function App() {
         }
         zoom={[13]}
         containerStyle={{
-          height: "100vh",
+          height: "80vh",
           width: "100vw"
         }}
       >
